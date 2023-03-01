5d0ad89f31edd0b9965ec41c806ac993c72e2de3
imtiaz1710 <imtiaz.mehedi.iubat@gmail.com>
2023-03-01 08:32:02 +0600

    sidebar and topic details modified

--- src/app/components/topic-details/topic-details.component.ts
+++ src/app/components/topic-details/topic-details.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { RoomService } from 'src/app/services/room.service';
 import { CreateRoomComponent } from '../create-room/create-room.component';
 
 @Component({
@@ -8,27 +9,54 @@ import { CreateRoomComponent } from '../create-room/create-room.component';
   templateUrl: './topic-details.component.html',
   styleUrls: ['./topic-details.component.css']
 })
-export class TopicDetailsComponent {
+export class TopicDetailsComponent implements OnInit {
   posts: any[] = [];
   topic: any;
-  constructor(private route: ActivatedRoute, private modalService: NgbModal, ) { }
+  id: string;
+  constructor(private route: ActivatedRoute, private modalService: NgbModal, private roomAService: RoomService) { }
 
   ngOnInit() {
-      // this.topic = { id: id, name: 'Python' };
-      this.posts = [
-        { id: 1, name: 'First Post', description: 'This is the first post', comments: [], showComment: false },
-        { id: 2, name: 'Second Post', description: 'This is the second post', comments: [], showComment: false },
-        { id: 3, name: 'First Post', description: 'This is the first post', comments: [], showComment: false },
-        { id: 4, name: 'Second Post', description: 'This is the second post', comments: [], showComment: false }
-      ];
+    debugger
+    // this.topic = { id: id, name: 'Python' };
+    this.id = this.route.snapshot.paramMap.get('id');
+debugger
+    if(this.id){
+      this.getRoomsByTopic(this.id);
+    }
+    else{
+      this.getAllRooms();
+    }
+
+    // this.posts = [
+    //   { id: 1, name: 'First Post', description: 'This is the first post', comments: [], showComment: false },
+    //   { id: 2, name: 'Second Post', description: 'This is the second post', comments: [], showComment: false },
+    //   { id: 3, name: 'First Post', description: 'This is the first post', comments: [], showComment: false },
+    //   { id: 4, name: 'Second Post', description: 'This is the second post', comments: [], showComment: false }
+    // ];
   }
 
-  showCommentToggle(post: any){
+  getRoomsByTopic(topic: any){
+    return this.roomAService.getRoomsByTopic(topic).subscribe({
+      next: (rooms) => {
+        this.posts = rooms;
+      }
+    });
+  }
+
+  getAllRooms() {
+    this.roomAService.getRooms().subscribe({
+      next: res => {
+        this.posts = res.data.map(x => { return { id: x.id, Comment: x.comment, name: x.name,
+           description: x.description, showComment: false } })
+      }
+    })
+  }
+  showCommentToggle(post: any) {
     post.showComment = !post.showComment;
   }
 
   addComment(post: any, comment: string) {
-    let x =<HTMLInputElement>document.getElementById(`comment${post.id}`);
+    let x = <HTMLInputElement>document.getElementById(`comment${post.id}`);
     post.comments.push(x.value);
     x.value = '';
   }
